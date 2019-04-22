8a3c21cac5647005eadc3a9234654afd22b7eb1b
Madison Hale <madhale@gmail.com>
2019-04-22 17:41:56 -0400

    Added Redux Form. Working to get form state fixed

--- src/containers/AddNewTarget.js
+++ src/containers/AddNewTarget.js
@@ -1,56 +1,118 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { reduxForm, Field, Form } from 'redux-form';
 import { connect } from 'react-redux';
 import { addNewTarget } from '../actions';
+import MenuItem from '@material-ui/core/MenuItem';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Select from '@material-ui/core/Select';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormHelperText from '@material-ui/core/FormHelperText';
 
-const AddNewTarget = ({ dispatch }) => {
-    let input;
+const RenderTextField = ({
+    label,
+    input,
+    meta: { touched, invalid, error },
+    ...custom
+}) => (
+    <TextField
+        label={label}
+        placeholder={label}
+        error={touched && invalid}
+        helperText={touched && error}
+        {...input}
+        {...custom}
+    />
+)
 
+// const RenderSelectField = ({
+//     input,
+//     label,
+//     meta: { touched, error },
+//     children,
+//     ...custom
+// }) => (
+//     <FormControl error={touched && error}>    
+//         <Select
+//         {...input}
+//         {...custom}
+//         inputProps={{
+//             name: 'status',
+//             id: 'status-simple'
+//         }}
+//         >
+//         {children}
+//         </Select>
+//         {renderFromHelper({ touched, error })}
+//     </FormControl>
+// )
+
+const RenderSelectField = (
+    { input, label, meta: { touched, error }, children, ...custom },
+  ) => (
+    <Select
+      native
+      label={label}
+      errortext={touched && error}
+      {...input}
+      onChange={(event, index, value) => input.onChange(value)}
+      children={children}
+      {...custom}
+    />
+  );
+
+const renderFromHelper = ({ touched, error }) => {
+    if (!(touched && error)) {
+      return
+    } else {
+      return <FormHelperText>{touched && error}</FormHelperText>
+    }
+  }
+
+const AddNewTarget = props => {
+    const {handleSubmit, submitting} = props;
     return (
-        <form className="NewTargetForm" onSubmit={e => {
-            e.preventDefault()
-            dispatch(addNewTarget(input))
-          }}>
-            <div className="formRow">
-                <label htmlFor="name">Enter company name</label>
-                <input id="name" name="name" type="text" ref={node => input = node} />
-            </div>
-            <div className="formRow">
-                <label htmlFor="description">Enter company description</label>
-                <input id="description" name="description" type="text" />
-            </div>
-            <div className="formRow">
-                <label htmlFor="location">Enter company location</label>
-                <input id="location" name="location" type="text" />
-            </div>
-            <div className="formRow">
-                <label htmlFor="status">Enter Acquisition Status</label>
-                <select id="status">
-                    <option value="">--Please choose an option--</option>
-                    <option value="researching">Researching</option>
-                    <option value="pending approval">Pending Approval</option>
-                    <option value="approved">Approved</option>
-                    <option value="declined">Declined</option>
-                </select>
-            </div>
-            <div className="formRow">
-                <label htmlFor="contacts">Enter Key Contact</label>
-                <input id="contacts" name="contacts" type="text" />
-            </div>
-            <div className="formRow">
-                <label htmlFor="valuation">Enter Financial Valuation</label>
-                <input id="valuation" name="valuation" type="text" />
-            </div>
-            <div className="formRow">
-                <label htmlFor="liquidity">Enter Financial Liquidity Ratio</label>
-                <input id="liquidity" name="liquidity" type="text" />
-            </div>
-            <div className="formRow">
-                <label htmlFor="EBITDA">Enter EBITDA (Earnings before interest, tax, depreciation and amortization)</label>
-                <input id="EBITDA" name="EBITDA" type="text" />
-            </div>
-            <button type="submit">Add new company</button>
-        </form>
+        <Form onSubmit={handleSubmit}>
+        <div><Field name="name" component={RenderTextField} placeholder="Enter company name" label="Name" /></div>
+        <div><Field name="description" component={RenderTextField} placeholder="Enter company description" label="Description" /></div>
+        <div><Field name="location" component={RenderTextField} placeholder="Enter company location" label="Location" /></div>
+        <div><Field name="status" component={RenderSelectField} label="Status">
+                <option />
+                <option value="researching">Researching</option>
+                <option value="pending">Pending Approval</option>
+                <option value="approved">Approved</option>
+                <option value="declined">Declined</option>
+            </Field></div>
+        {/* <div><FormControl>
+          <InputLabel htmlFor="status-simple">Status</InputLabel>
+          <Select
+            value={this.state.status}
+            onChange={this.handleChange}
+            inputProps={{
+              name: 'status',
+              id: 'status-simple',
+            }}
+          >
+            <MenuItem value="">
+              <em>None</em>
+            </MenuItem>
+            <MenuItem value="researching">Researching</MenuItem>
+            <MenuItem value="pending">Pending Approval</MenuItem>
+            <MenuItem value="approved">Approved</MenuItem>
+            <MenuItem value="declined">Declined</MenuItem>
+          </Select>
+        </FormControl></div> */}
+        <div><Field name="contact" component={RenderTextField} placeholder="Enter Key Contact" label="Contact" /></div>
+        <div><Field name="valuation" component={RenderTextField} placeholder="Enter Financial Valuation" label="Valuation" /></div>
+        <div><Field name="liquidity" component={RenderTextField} placeholder="Enter Financial Liquidity Ratio" label="Liquidity" /></div>
+        <div><Field name="EBITDA" component={RenderTextField} placeholder="Enter EBITDA (Earnings before interest, tax, depreciation and amortization)" label="EBITDA" /></div>
+        <div><button type="submit" disabled={submitting}>Add new company</button></div>
+        </Form>
     )
 }
 
-export default connect()(AddNewTarget)
+
+export default reduxForm({
+    form: 'acquisitionTarget', // a unique identifier for this form
+  })(AddNewTarget);
--- src/index.js
+++ src/index.js
@@ -5,8 +5,53 @@ import { Provider } from 'react-redux'
 import './index.css';
 import App from './components/App';
 import rootReducer from './reducers'
+import { reduxForm } from 'redux-form'
 
-const store = createStore(rootReducer)
+const initialState = {
+    companies: [
+      {
+        "id": 1,
+        "name": "Sprint",
+        "description": "American telecommunications company",
+        "location": "Overland Park, Kansas",
+        "status": "pending approval",
+        "contacts": "John Smith",
+        "finances": {
+          "valuation": "25.5B",
+          "liquidity": "1.16",
+          "EBITDA": "13.13B"
+        }
+      },
+      {
+        "id": 2,
+        "name": "Fox Corporation",
+        "description": "American television broadcasting company",
+        "location": "New York City, New York",
+        "status": "declined",
+        "contacts": "Alan Jackson",
+        "finances": {
+          "valuation": "71B",
+          "liquidity": "3.17",
+          "EBITDA": "2.54B"
+        }
+      },
+      {
+        "id": 3,
+        "name": "Coca-Cola Co",
+        "description": "American multinational corporation",
+        "location": "Atlanta, Georgia",
+        "status": "researching",
+        "contacts": "Brian Jefferson",
+        "finances": {
+          "valuation": "204B",
+          "liquidity": "1.05",
+          "EBITDA": "10.72B"
+        }
+      }
+    ]
+  };
+
+const store = createStore(rootReducer, initialState)
 
 render(
     <Provider store={store}>
